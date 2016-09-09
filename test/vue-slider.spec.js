3599eda0367f52b21e34baa0c411e5c2b1843486
lakb248 <lakb248@gmail.com>
2016-09-09 16:13:10 +0800

    update unit test

--- test/vue-slider.spec.js
+++ test/vue-slider.spec.js
@@ -1,4 +1,4 @@
-/* global describe, it, expect, document */
+/* global describe, it, expect, jasmine, beforeEach, afterEach, document */
 import Vue from 'vue';
 import VueSlider from '../src/vue-slider.vue';
 import Slider from '../src/slider.vue';
@@ -6,7 +6,8 @@ import Slider from '../src/slider.vue';
 var initSlider = () => {
     return new Vue({
         template: '<div style="width:200px;height:50px;">' +
-            '<vue-slider :speed="1000" :arrow="true" :dot="true">' +
+            '<vue-slider v-ref:slider :speed="400" ' +
+                ':arrow="true" :dot="true" :auto="2000">' +
                 '<slider-item>' +
                     '<div class="slider" style="display:none' +
                     ';background-color:red;height:100%;">SliderA</div>' +
@@ -28,9 +29,38 @@ var initSlider = () => {
     }).$mount().$appendTo('body');
 };
 describe('vue-slider.vue', () => {
+    var originalTimeout;
+    beforeEach(() => {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+    });
+    afterEach(() => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
     it('should have three slider after render', () => {
         initSlider();
         expect(document.querySelectorAll('.slider').length).toBe(3);
+    });
+    it('should transfer to the second slider after 3000ms', done => {
+        var vm = initSlider();
+        setTimeout(() => {
+            expect(vm.$refs.slider.index).toBe(1);
+            done();
+        }, 2200);
+    });
+    it('should transfer to the third slider after 6000ms', done => {
+        var vm = initSlider();
+        setTimeout(() => {
+            expect(vm.$refs.slider.index).toBe(2);
+            done();
+        }, 4800);
+    });
+    it('should transfer to the first slider after 9000ms', done => {
+        var vm = initSlider();
+        setTimeout(() => {
+            expect(vm.$refs.slider.index).toBe(0);
+            done();
+        }, 7400);
     });
 });
 
