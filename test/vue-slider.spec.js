ecdcbbbb7c9188c406a476b9d3abaa67fc555c22
lakb248 <lakb248@gmail.com>
2016-09-09 15:46:52 +0800

    add unit test

--- test/vue-slider.spec.js
+++ test/vue-slider.spec.js
@@ -1,3 +1,37 @@
-/* global describe */
+/* global describe, it, expect, document */
+import Vue from 'vue';
+import VueSlider from '../src/vue-slider.vue';
+import Slider from '../src/slider.vue';
 
-describe('vue-slider.vue', () => {});
+var initSlider = () => {
+    return new Vue({
+        template: '<div style="width:200px;height:50px;">' +
+            '<vue-slider :speed="1000" :arrow="true" :dot="true">' +
+                '<slider-item>' +
+                    '<div class="slider" style="display:none' +
+                    ';background-color:red;height:100%;">SliderA</div>' +
+                '</slider-item>' +
+                '<slider-item>' +
+                    '<div class="slider" style="display:none' +
+                    ';background-color:red;height:100%;">SliderA</div>' +
+                '</slider-item>' +
+                '<slider-item>' +
+                    '<div class="slider" style="display:none' +
+                    ';background-color:red;height:100%;">SliderA</div>' +
+                '</slider-item>' +
+            '</vue-slider>' +
+        '</div>',
+        components: {
+            'vue-slider': VueSlider,
+            'slider-item': Slider
+        }
+    }).$mount().$appendTo('body');
+};
+describe('vue-slider.vue', () => {
+    it('should have three slider after render', () => {
+        initSlider();
+        expect(document.querySelectorAll('.slider').length).toBe(3);
+    });
+});
+
+// describe('slider.vue', () => {});
