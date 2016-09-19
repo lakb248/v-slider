ab360cb3970fce62f160d4a131ccdd1d9ef4d953
lakb248 <lakb248@gmail.com>
2016-09-19 09:29:56 +0800

    add codecov

--- karma.config.js
+++ karma.config.js
@@ -25,8 +25,8 @@ module.exports = function(config) {
             dir: './coverage',
             reporters: [
                 {type: 'html'},
-                {type: 'text-summary'}
-                // {type: 'cobertura', subdir: '.'}
+                {type: 'text-summary'},
+                {type: 'cobertura', subdir: '.'}
             ]
         },
         webpack: webpackConfig,
